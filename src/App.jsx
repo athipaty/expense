174f76f09637aa49d0f6fd90d586b193af0d8469
Loretta Yong <loretta0386@hotmail.com>
2026-05-12 00:33:58 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -214,7 +214,7 @@ export default function App() {
               No income logged this month
             </p>
           )}
-          {income.map((inc) => (
+          {[...income].sort((a, b) => b.amount - a.amount).map((inc) => (
             <div
               key={inc._id}
               className="bg-gray-900 rounded-xl px-4 py-3 flex items-center justify-between"
